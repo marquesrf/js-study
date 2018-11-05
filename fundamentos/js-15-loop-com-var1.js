b2b6c9812069f3921d15f805afe01ed47d127420
Rafael Marques <rafa.marques.learn@gmail.com>
2018-11-05 11:42:58 -0300

    loops with var and let

---
+++ fundamentos/js-15-loop-com-var1.js
@@ -0,0 +1,6 @@
+// a variável fica visível fora do escopo
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log('i = ', i);
---
+++ fundamentos/js-16-loop-com-let.js
@@ -0,0 +1,6 @@
+// a variável NÃO fica visível fora do escopo
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log('i = ', i);
