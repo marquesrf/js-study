e3e444d0a84bc13d50fb4f2772cfa4bc7c7acace
Rafael Marques <rafa.marques.learn@gmail.com>
2019-01-15 11:30:01 -0300

    code regarding NODE lessons

---
+++ 6-node/funcionarios.js
@@ -0,0 +1,25 @@
+/**
+ * Dada uma lista(url) com 500 funcionários, encontre a mulher chinesa com o menor salário.
+ * @param {*} f 
+ */
+
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+const chineses = f => f.pais === 'China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+    // console.log(funcionarios)
+
+    const func = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+
+    console.log(func)
+})
